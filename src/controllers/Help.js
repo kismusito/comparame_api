6caec328d74f4dd723d381891a1dd4812401d39a
Guille <guilleccubillos@hotmail.com>
2021-04-26 23:38:00 -0500

    Endpoints Products - CRUD 5/5 - EnvMail 0/1

--- src/controllers/Help.js
+++ src/controllers/Help.js
@@ -69,51 +69,58 @@ ProductMethods.updateProduct = async (req, res) => {
                 if (ProductID) {                    
                     const checkSupermarker = await Supermarker.findById(SupermarkerID);
                     const checkProduct = await Product.findById(ProductId);
-                    if (checkSupermarker == checkProduct.supermarker) {
-                        
+                    if (checkSupermarker._id == checkProduct.supermarker) {
+                        const checkNewCategory = await Category.findById(categoryID);
+                        if(checkNewCategory){
+                            const checkProductName = await Product.find({product_name: product_name, supermarker: Supermarker.findById(supermarkerID)});
+                            if(checkProductName){
+                                return res.status(400).json({
+                                    status: false,
+                                    message: "El Nombre del producto ya esta en uso.",
+                                });
+                            }
+                            const newProduct = await checkProduct.updateOne({
+                                categories : categoryID,
+                                product_name : product_name,
+                                product_price : product_price,
+                                product_status : product_status,
+                                product_discount : product_discount,
+                                product_feautered : product_feautered,
+                            });
+                            if (newProduct) {
+                                return res.status(201).json({
+                                    status: true,
+                                    message: "El Producto ha sido actualizado correctamente.",
+                                });
+                            } else {
+                                return res.status(405).json({
+                                    status: false,
+                                    message:
+                                        "Ha ocurrido un error, por favor intentalo nuevamente.",
+                                });
+                            }
+                        }else{
+                            return res.status(400).json({
+                                status: false,
+                                message: "La Categoria a la que intenta cambiar no existe'",
+                            });
+                        }
                     }else{
                         return res.status(400).json({
                             status: false,
                             message: "Este producto no pertenece a dicho supermercado",
                         });
                     }
-
-
-
-                    if (checkProduct) {
-                        const CheckCategories = await Category.findById(categoryID)
-                        if (
-                            await getRol.updateOne({
-                                rolName: rol_name,
-                            })
-                        ) {
-                            return res.status(201).json({
-                                status: false,
-                                message: "El rol ha sido actualizado correctamente.",
-                            });
-                        } else {
-                            return res.status(405).json({
-                                status: false,
-                                message:
-                                    "Ha ocurrido un error, por favor intentalo nuevamente.",
-                            });
-                        }    
-                    } else {
-                        return res.status(400).json({
-                            status: false,
-                            message: "El nombre del rol no esta permitido.",
-                        });
-                    }
                 } else {
                     return res.status(400).json({
                         status: false,
-                        message: "El nombre del rol es requerido.",
+                        message: "No se ah encontrado el producto.",
                     });
                 }
             } else {
                 return res.status(400).json({
                     status: false,
-                    message: "El id rel rol es requerido.",
+                    message: "No se encontro Supermercado",
                 });
             }
         } else {
@@ -130,7 +137,61 @@ ProductMethods.updateProduct = async (req, res) => {
     }
 };
 
-ProductMethods.deleteProduct = async (req, res) => {};
+ProductMethods.deleteProduct = async (req, res) => {
+    try {
+        const permissions = Permission.can(req.user.rol.name).deleteOwn("product").granted
+        if (permissions) {
+            const { ProductID } = req.body;
+            if (ProductID) {
+                const getProduct = await Product.findById(ProductID);
+                if (getProduct) {
+                    const checkCategorProduct = await Category.find(
+                        { products: ProductID },
+                        { _id: true }
+                    );
+                    if (checkCategorProduct.length > 0) {
+                        return res.status(400).json({
+                            status: false,
+                            message:
+                                "Este Producto esta en una categoria, no puedes eliminarlo.",
+                        });
+                    }
+                    if (await getProduct.remove()) {
+                        return res.status(200).json({
+                            status: true,
+                            message: "El rol ha sido eliminado correctamente.",
+                        });
+                    } else {
+                        return res.status(405).json({
+                            status: false,
+                            message: "Ha ocurrido un error, por favor intentalo nuevamente.",
+                        });
+                    }
+                } else {
+                    return res.status(400).json({
+                        status: false,
+                        message: "El Producto no ha sido encontrado.",
+                    });
+                }
+            } else {
+                return res.status(400).json({
+                    status: false,
+                    message: "El id del producto es requerido.",
+                });
+            }
+        } else {
+            return res.status(400).json({
+                status: false,
+                message: "No tienes permiso para acceder a este recurso.",
+            });
+        }
+    } catch (error) {
+        return res.status(405).json({
+            status: false,
+            message: "Ha ocurrido un error, por favor intentalo nuevamente.",
+        });
+    }
+};
 
 ProductMethods.createProduct = async (req, res) => {
     try {
@@ -189,7 +250,7 @@ ProductMethods.createProduct = async (req, res) => {
                         });
                         if (await product.save()) {
                             return res.status(201).json({
-                                status: false,
+                                status: true,
                                 product,
                                 message: "Ha creado un nuevo producto satisfactoriamente.",
                             });
